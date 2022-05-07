46f450380952994dfb130e94bed9f2b85553e76e
PhuTruongThanh <phutt.15.student@fit.tdc.edu.vn>
2022-05-07 10:42:25 +0700

    b111 update2

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'b57d7400951d91e3364928d3f5835bd5320b5626',
+        'name' => 'phuthanhtruong/package2',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phuthanhtruong/package2' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'b57d7400951d91e3364928d3f5835bd5320b5626',
+            'dev_requirement' => false,
+        ),
+        'phutruongthanh/package2' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phutruongthanh/package2',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => 'b57d7400951d91e3364928d3f5835bd5320b5626',
+            'dev_requirement' => false,
+        ),
+    ),
+);
